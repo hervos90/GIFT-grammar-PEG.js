63ce51ca648dd9334af26d3afdd3f295773a7c35
Hervos90 <hervebogne@gmail.com>
2026-04-25 11:28:53 -0400

    Update global tests

--- tests/all-questions-harness.js
+++ tests/all-questions-harness.js
@@ -8,6 +8,22 @@ const files = glob.sync(questionsFolder + '*.gift', { nonull: true });
 
 const parserGeneratedFromGrammar = peg.generate(fs.readFileSync('GIFT.pegjs', 'utf-8'));
 
+// Helper function to remove functions from objects for JSON comparison
+function stripFunctions(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map(stripFunctions);
+  } else if (obj !== null && typeof obj === 'object') {
+    const cleaned = {};
+    for (const key in obj) {
+      if (typeof obj[key] !== 'function') {
+        cleaned[key] = stripFunctions(obj[key]);
+      }
+    }
+    return cleaned;
+  }
+  return obj;
+}
+
 describe(`GIFT question types`, () => {
     files.forEach((file) => {
 
@@ -17,9 +33,9 @@ describe(`GIFT question types`, () => {
             const jsonText = fs.readFileSync(jsonFile, 'utf-8');
             const jsonParse = JSON.parse(jsonText);
             const parsing1 = parserGeneratedFromGrammar.parse(giftText);
-            expect(parsing1).toEqual(jsonParse);
+            expect(stripFunctions(parsing1)).toEqual(jsonParse);
             const parsing2 = parse(giftText);
-            expect(parsing2).toEqual(jsonParse);
+            expect(stripFunctions(parsing2)).toEqual(jsonParse);
         });
     });
 });
